644ee6126f1c11b5dd853c44052d8351228ab2c1
Aleksandr Gladkikh <20492198+galexla@users.noreply.github.com>
2024-08-05 23:03:53 +0300

    ChatGPT 23:03

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 import commands
 from db.dals import get_reminders_after_date, set_reminder_sent
 from middlewares import DbSessionMiddleware
-from scheduler import initialize_scheduler, scheduler
+from scheduler import initialize_scheduler, run_async_job, scheduler
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger("main")
@@ -18,7 +18,6 @@ log = logging.getLogger("main")
 load_dotenv(".env")
 TG_API_TOKEN = os.environ.get("TG_API_TOKEN")
 DB_URL = os.environ.get("DB_URL")
-SYNC_DB_URL = os.environ.get("SYNC_DB_URL")
 
 bot = None
 
@@ -52,8 +51,10 @@ async def main():
     dp.update.middleware(DbSessionMiddleware(session_pool=sessionmaker))
     dp.include_router(commands.router)
 
-    initialize_scheduler(SYNC_DB_URL)
-    scheduler.add_job(send_reminders, "interval", seconds=10)
+    initialize_scheduler(DB_URL)
+    scheduler.add_job(
+        lambda: run_async_job(send_reminders), "interval", seconds=10
+    )
 
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,13 @@
-from apscheduler.executors.pool import ProcessPoolExecutor
+import asyncio
+
+from apscheduler.executors.pool import ThreadPoolExecutor
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pytz import utc
 from sqlalchemy import create_engine
 
 jobstores = {}
-executors = {"default": ProcessPoolExecutor(2)}
+executors = {"default": ThreadPoolExecutor(1)}
 job_defaults = {
     "coalesce": True,
 }
@@ -26,3 +28,8 @@ def initialize_scheduler(db_url: str):
         timezone=utc,
     )
     scheduler.start()
+
+
+def run_async_job(coroutine_func):
+    loop = asyncio.get_event_loop()
+    loop.create_task(coroutine_func)
