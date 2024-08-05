1ee74f477689724f59b0f33016721e619013fa27
Aleksandr Gladkikh <20492198+galexla@users.noreply.github.com>
2024-08-05 22:55:58 +0300

    ChatGPT 22:55

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 import commands
 from db.dals import get_reminders_after_date, set_reminder_sent
 from middlewares import DbSessionMiddleware
-from scheduler import scheduler
+from scheduler import initialize_scheduler, scheduler
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger("main")
@@ -18,12 +18,12 @@ log = logging.getLogger("main")
 load_dotenv(".env")
 TG_API_TOKEN = os.environ.get("TG_API_TOKEN")
 DB_URL = os.environ.get("DB_URL")
+SYNC_DB_URL = os.environ.get("SYNC_DB_URL")
 
 bot = None
 
 
 async def send_reminders():
-    log.debug("send_reminders()")
     async with async_sessionmaker(create_async_engine(DB_URL))() as session:
         log.debug("Sending reminders...")
         reminders = await get_reminders_after_date(
@@ -41,12 +41,6 @@ async def send_reminders():
         log.info("Sent %s reminder(s)", len(reminders))
 
 
-async def start_scheduler():
-    # job = scheduler.add_job(send_reminders, "interval", minutes=1)
-    job = scheduler.add_job(send_reminders, "interval", seconds=10)
-    scheduler.start()
-
-
 async def main():
     engine = create_async_engine(url=DB_URL, echo=True)
     sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
@@ -58,7 +52,8 @@ async def main():
     dp.update.middleware(DbSessionMiddleware(session_pool=sessionmaker))
     dp.include_router(commands.router)
 
-    asyncio.create_task(start_scheduler())
+    initialize_scheduler(SYNC_DB_URL)
+    scheduler.add_job(send_reminders, "interval", seconds=10)
 
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
--- scheduler.py
+++ scheduler.py
@@ -2,17 +2,27 @@ from apscheduler.executors.pool import ProcessPoolExecutor
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pytz import utc
+from sqlalchemy import create_engine
 
-jobstores = {
-    "default": SQLAlchemyJobStore(url="sqlite+aiosqlite:///jobs.sqlite")
-}
+jobstores = {}
 executors = {"default": ProcessPoolExecutor(2)}
 job_defaults = {
     "coalesce": True,
 }
-scheduler: AsyncIOScheduler = AsyncIOScheduler(
-    jobstores=jobstores,
+scheduler = AsyncIOScheduler(
     executors=executors,
     job_defaults=job_defaults,
     timezone=utc,
 )
+
+
+def initialize_scheduler(db_url: str):
+    sync_engine = create_engine(db_url)
+    jobstores["default"] = SQLAlchemyJobStore(engine=sync_engine)
+    scheduler.configure(
+        jobstores=jobstores,
+        executors=executors,
+        job_defaults=job_defaults,
+        timezone=utc,
+    )
+    scheduler.start()
